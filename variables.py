e6641ee8a96295429407dca7c9739136a854ff4a
Atishu242 <atikullash085@gmail.com>
2025-04-28 02:43:30 +0600

    initial commit

---
+++ variables.py
@@ -0,0 +1,73 @@
+#Problem 1 = creating variables for favorite food and drink
+favorite_food = "Biriyani"
+favorite_drink = "lemon tea"
+
+#printing the sentence
+print(f"My favorite food is {favorite_food} and my favorite drink is {favorite_drink}.")
+
+
+#Problem 2 = assigining values to a and b
+a=6
+b=8
+
+#calculating the sum, substration, multiplication and division of a and b
+sum = a + b
+substraction = a - b
+multiplication = a * b
+division = a / b
+
+#printing the results
+print(f"Sum = {sum}")
+print(f"Substraction = {substraction}")
+print(f"Multiplication = {multiplication}")
+print(f"Division = {division}")
+
+
+#problem 3 = swap values of a and b
+
+#assigning values to a and b
+a = 6
+b = 7
+
+#swapping values of a and b
+a,b=b,a
+
+#printing the final values
+print(f"Final values: \na={a},\nb={b}")
+
+
+#problem 4: creating my bio
+
+#creating variables for my bio
+name = "Ullash"
+age = 24
+city = "Dhaka"
+hobby = "learning python"
+
+#printing my bio
+print(f"hi, my name is {name}, I am {age} years old, I live in {city} and my enjoy {hobby}")
+
+
+#problem 5: Variable Reassignment
+
+#creating a variable named mood and printing it
+
+mood= "happy"
+print(mood)
+
+#changing the value to excited and printing it
+mood= "excited"
+print(mood)
+
+
+#problem 6: Special problem(pet information)
+
+#assigning variables
+name= "Coco"
+animal_type= "dog"
+age= "3"
+favorite_toy= "ball"
+
+#printing the sentence
+print(f"My pet {name} is a {animal_type}. They are {age} years old and love playing with their {favorite_toy}.")
+
